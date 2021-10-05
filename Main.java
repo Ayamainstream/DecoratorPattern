70db1acf874c399f9ff17a863de9e7a6d7baccd9
Ayamainstream <72498812+Ayamainstream@users.noreply.github.com>
2021-10-05 23:52:40 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Ticket ticket = new TicketDecorator((new TicketWithBaggage(new SimpleTicket())));
+        Ticket ticket = new TicketWithBaggage(new SimpleTicket());
         System.out.println("Your ticket is: " + ticket.getDescription());
         System.out.println("Cost: " + ticket.getCost());
 
-        Ticket ticket2 = new TicketDecorator((new VipTicket(new SimpleTicket())));
+        Ticket ticket2 = new VipTicket(new SimpleTicket());
 
         System.out.println("Your ticket is: " + ticket2.getDescription());
         System.out.println("Cost: " + ticket2.getCost());
